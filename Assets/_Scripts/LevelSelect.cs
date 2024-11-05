780dc7366f88753e69626dae3431ccbe712b2d4b
DESKTOP-V7INGJK\trungpc <trungpc100@gmail.com>
2024-11-05 21:47:42 +0700

    add level

---
+++ Assets/_Scripts/LevelSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+   
+    public GameObject playPanel; 
+
+    private int selectedLevel;
+
+    public void SelectLevel(int level)
+    {
+        selectedLevel = level; // Gán màn chơi đã chọn
+        
+        playPanel.SetActive(true); // Hiện panel chơi
+    }
+
+    public void PlaySelectedLevel()
+    {
+        SceneManager.LoadScene("Level" + selectedLevel); // Chuyển đến scene đã chọn
+    }
+
+    public void GoBack()
+    {
+        playPanel.SetActive(false); // Ẩn panel chơi
+       // Hiện lại panel chọn màn chơi
+    }
+}
