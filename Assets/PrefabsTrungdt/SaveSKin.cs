2f9b1268e56e94a60dbc2ef66427cc1f43a0f314
moderkaitolyasuo <moderkaitolyasuo@gmail.com>
2024-12-07 12:42:41 +0700

    fix error

--- Assets/PrefabsTrungdt/SaveSKin.cs
+++ Assets/PrefabsTrungdt/SaveSKin.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class SaveSkin : MonoBehaviour
 {
-   
     [SerializeField] private SpriteRenderer hatRenderer;
     [SerializeField] private SpriteRenderer faceRenderer;
     [SerializeField] private SpriteRenderer shirtRenderer;
@@ -10,8 +9,6 @@ public class SaveSkin : MonoBehaviour
     [SerializeField] private SpriteRenderer gunRenderer;
     [SerializeField] private SpriteRenderer colorRenderer;
 
-    
-    [SerializeField] private ItemDatabase itemDatabase;
 
     private void Start()
     {
@@ -32,42 +29,42 @@ public class SaveSkin : MonoBehaviour
         // Cập nhật mũ
         if (selectedHatID != -1)
         {
-            ItemData hatData = itemDatabase.ItemsHat.Find(item => item.ID == selectedHatID);
+            ItemData hatData = ItemDatabase.Instance.ItemsHat.Find(item => item.ID == selectedHatID);
             if (hatData != null) hatRenderer.sprite = hatData.Icon;
         }
 
         // Cập nhật mặt
         if (selectedFaceID != -1)
         {
-            ItemData faceData = itemDatabase.ItemsFace.Find(item => item.ID == selectedFaceID);
+            ItemData faceData = ItemDatabase.Instance.ItemsFace.Find(item => item.ID == selectedFaceID);
             if (faceData != null) faceRenderer.sprite = faceData.Icon;
         }
 
         // Cập nhật áo
         if (selectedShirtID != -1)
         {
-            ItemData shirtData = itemDatabase.Itemsshirt.Find(item => item.ID == selectedShirtID);
+            ItemData shirtData = ItemDatabase.Instance.Itemsshirt.Find(item => item.ID == selectedShirtID);
             if (shirtData != null) shirtRenderer.sprite = shirtData.Icon;
         }
 
         // Cập nhật body
         if (selectedBodyID != -1)
         {
-            ItemData bodyData = itemDatabase.Itemsbody.Find(item => item.ID == selectedBodyID);
+            ItemData bodyData = ItemDatabase.Instance.Itemsbody.Find(item => item.ID == selectedBodyID);
             if (bodyData != null) bodyRenderer.sprite = bodyData.Icon;
         }
 
         // Cập nhật súng
         if (selectedGunID != -1)
         {
-            ItemData gunData = itemDatabase.Itemsgun.Find(item => item.ID == selectedGunID);
+            ItemData gunData = ItemDatabase.Instance.Itemsgun.Find(item => item.ID == selectedGunID);
             if (gunData != null) gunRenderer.sprite = gunData.Icon;
         }
 
         // Cập nhật màu sắc (nếu cần đổi màu thay vì sprite)
         if (selectedColorID != -1)
         {
-            ItemData colorData = itemDatabase.Itemscolor.Find(item => item.ID == selectedColorID);
+            ItemData colorData = ItemDatabase.Instance.Itemscolor.Find(item => item.ID == selectedColorID);
             if (colorData != null) colorRenderer.color = colorData.ItemColor; // Đổi màu trực tiếp
         }
     }
---
+++ Assets/_Scripts/SO/ItemDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using GameTool.Assistants.DictionarySerialize;
+using UnityEngine;
+
+public class ItemDatabase : ScriptableObject
+{
+    public static ItemDatabase Instance => Resources.Load<ItemDatabase>("SO/ItemDatabase"); 
+    
+    public List<ItemData> ItemsHat;
+    public List<ItemData> ItemsFace;
+    public List<ItemData> Itemsshirt;
+    public List<ItemData> Itemsbody;
+    public List<ItemData> Itemsgun;
+    public List<ItemData> Itemscolor;
+    public List<ItemData> Itemsperk;
+}
+
+public class ItemData
+{
+    public int ID;
+    public ItemController.ItemType Type;
+    public Sprite Icon;
+    public Color ItemColor;
+}
